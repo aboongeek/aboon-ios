a2346c4a39b370e21662d089ab2f5d729372a6d4
kichie <jubeater_618@yahoo.co.jp>
2018-06-01 18:53:36 +0900

    Initial Commit

---
+++ aboon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aboon
+//
+//  Created by 吉江勇亮 on 2018/06/01.
+//  Copyright © 2018年 aboon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
