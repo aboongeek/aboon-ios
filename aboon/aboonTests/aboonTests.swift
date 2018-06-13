189aec0463ca79a871d20fa4e39e7a5cbec83bd3
kichie <jubeater_618@yahoo.co.jp>
2018-06-13 20:43:34 +0900

    init commit

---
+++ aboon/aboonTests/aboonTests.swift
@@ -0,0 +1,36 @@
+//
+//  aboonTests.swift
+//  aboonTests
+//
+//  Created by 吉江勇亮 on 2018/06/13.
+//  Copyright © 2018年 aboon. All rights reserved.
+//
+
+import XCTest
+@testable import aboon
+
+class aboonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
