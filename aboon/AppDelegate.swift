86d05a385045a5c38e71c98eeda86f4240ff3e57
Kazune <kazune.haraguchi@gmail.com>
2018-06-21 13:07:20 +0900

    Initialized Firebase

--- aboon/AppDelegate.swift
+++ aboon/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
