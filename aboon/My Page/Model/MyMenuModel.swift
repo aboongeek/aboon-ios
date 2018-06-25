a20979fb42793b1a7278cf61b202df64c059f1f4
Kazune <kazune.haraguchi@gmail.com>
2018-06-25 21:30:41 +0900

    added viewcontroller for side menu (my menu)

---
+++ aboon/My Page/Model/MyMenuModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyMenuModel.swift
+//  aboon
+//
+//  Created by 原口和音 on 2018/06/25.
+//  Copyright © 2018 aboon. All rights reserved.
+//
+
+import UIKit
+
+class MyMenuModel {
+    @objc public func showMyMenu() {
+        
+    }
+}
