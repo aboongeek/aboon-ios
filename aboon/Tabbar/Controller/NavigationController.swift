b65fa0c142b6e6759a633c2202477fc014c4d27e
Kazune <kazune.haraguchi@gmail.com>
2018-06-25 21:36:54 +0900

    imported SideMenu removed unnecessary comments

--- aboon/Tabbar/Controller/NavigationController.swift
+++ aboon/Tabbar/Controller/NavigationController.swift
@@ -7,13 +7,12 @@
 //
 
 import UIKit
+import SideMenu
 
 class NavigationController: UINavigationController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
