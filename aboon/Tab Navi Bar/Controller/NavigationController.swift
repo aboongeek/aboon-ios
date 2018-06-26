3ffcfef7f3d19c597bb0f913720cbaddc2836106
Kazune <kazune.haraguchi@gmail.com>
2018-06-26 23:38:06 +0900

    [update] moved the configuration function of navigation bar and side menu to NavigationController and NavigationItem classes

---
+++ aboon/Tab Navi Bar/Controller/NavigationController.swift
@@ -0,0 +1,33 @@
+//
+//  NavigationController.swift
+//  aboon
+//
+//  Created by 原口和音 on 2018/06/25.
+//  Copyright © 2018 aboon. All rights reserved.
+//
+
+import UIKit
+import SideMenu
+
+class NavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sideMenuController = UISideMenuNavigationController(rootViewController: MyMenuController())
+        SideMenuManager.default.menuRightNavigationController = sideMenuController
+        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationBar)
+        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.view, forMenu: .right )
+        SideMenuManager.default.menuPresentMode = .menuSlideIn
+    }
+    
+    @objc public func showMyMenu() {
+        present(SideMenuManager.default.menuRightNavigationController!, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+}
