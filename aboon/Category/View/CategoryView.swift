cfa66aedde0fc6ad05260156640602376d046bef
Kazune <kazune.haraguchi@gmail.com>
2018-06-27 15:25:47 +0900

    [update] split CategoryCollectionView class from CategoryView.swift to a new file

--- aboon/Category/View/CategoryView.swift
+++ aboon/Category/View/CategoryView.swift
@@ -41,17 +41,4 @@ class CategoryView: UIView {
 
 }
 
-class CategoryCollectionView: UICollectionView {
-    
-    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
-        super.init(frame: frame, collectionViewLayout: layout)
-        self.backgroundColor = .white
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-}
 
