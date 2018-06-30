34313c152ca5a49cea45e3e138c502ad2c889382
kamuri sengoku <nekotokotatsu.nyaaaaa@gmail.com>
2018-06-30 11:45:16 +0900

    added couponlistcollectionview cell

---
+++ aboon/Category/View/Coupon List View/CouponListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CouponListCollectionViewCell.swift
+//  aboon
+//
+//  Created by John Titer on H30/06/30.
+//  Copyright © 平成30年 aboon. All rights reserved.
+//
+
+import UIKit
+
+class CouponListCollectionViewCell: UICollectionViewCell {
+    
+}
