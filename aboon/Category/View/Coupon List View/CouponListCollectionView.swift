9b9c99bba4d63b64ab1f6a44c77646ddb276efe1
kamuri sengoku <nekotokotatsu.nyaaaaa@gmail.com>
2018-06-30 12:30:11 +0900

    edited couponlistcollectionviewcell

--- aboon/Category/View/Coupon List View/CouponListCollectionView.swift
+++ aboon/Category/View/Coupon List View/CouponListCollectionView.swift
@@ -10,4 +10,12 @@ import UIKit
 
 class CouponListCollectionView: UICollectionView {
 
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- aboon/Category/View/Coupon List View/CouponListCollectionViewCell.swift
+++ aboon/Category/View/Coupon List View/CouponListCollectionViewCell.swift
@@ -10,4 +10,37 @@ import UIKit
 
 class CouponListCollectionViewCell: UICollectionViewCell {
     
+    var couponImageView: UIImageView?
+    var couponNameLabel: UILabel?
+    var couponDetailLabel: UILabel?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .gray
+        //クーポンに紐付ける画像の初期化
+        couponImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        addSubview(couponImageView!)
+        //クーポンタイトルの初期化
+        couponNameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        couponNameLabel?.textAlignment = .left
+        couponNameLabel?.textColor = .black
+        couponNameLabel?.font = UIFont.boldSystemFont(ofSize: (couponNameLabel?.font.pointSize)!)
+        couponNameLabel?.text = "nil"
+        
+        addSubview(couponNameLabel!)
+        //クーポンの説明文の初期化
+        couponDetailLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        couponDetailLabel?.textAlignment = .left
+        couponDetailLabel?.textColor = .black
+        couponDetailLabel?.font = UIFont.boldSystemFont(ofSize: (couponDetailLabel?.font.pointSize)!)
+        couponDetailLabel?.text = "nil"
+        
+        addSubview((couponDetailLabel!))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
