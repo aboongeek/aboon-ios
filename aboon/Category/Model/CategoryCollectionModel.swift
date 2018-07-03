03bc730e8f32e9dc65e98e4efb55d5394081fd62
原口和音 <kazunesaiko@yahoo.co.jp>
2018-07-03 17:56:47 +0900

    [remove] removed a method which delays for serial processings

--- aboon/Category/Model/CategoryCollectionModel.swift
+++ aboon/Category/Model/CategoryCollectionModel.swift
@@ -71,25 +71,3 @@ extension CategoryCollectionModel: UICollectionViewDataSource {
         return cell
     }
 }
-
-extension CategoryCollectionModel {
-    func wait(_ waitContinuation: @escaping (()->Bool), compleation: @escaping (()->Void)) {
-        var wait = waitContinuation()
-        // 0.01秒周期で待機条件をクリアするまで待ちます。
-        let semaphore = DispatchSemaphore(value: 0)
-        DispatchQueue.global().async {
-            while wait {
-                DispatchQueue.main.async {
-                    wait = waitContinuation()
-                    semaphore.signal()
-                }
-                semaphore.wait()
-                Thread.sleep(forTimeInterval: 0.01)
-            }
-            // 待機条件をクリアしたので通過後の処理を行います。
-            DispatchQueue.main.async {
-                compleation()
-            }
-        }
-    }
-}
