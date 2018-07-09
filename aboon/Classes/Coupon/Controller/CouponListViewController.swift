33a2495ac721873bf7faf9777a896e451ecb6b88
原口和音 <kazunesaiko@yahoo.co.jp>
2018-07-09 15:15:47 +0900

    [add] called setFrame to set up the frame size

--- aboon/Classes/Coupon/Controller/CouponListViewController.swift
+++ aboon/Classes/Coupon/Controller/CouponListViewController.swift
@@ -30,6 +30,7 @@ class CouponListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.setFrame(tabBar: (tabBarController?.tabBar)!, navBar: navigationController?.navigationBar as! NavigationBar)
         self.navigationItem.title = titleName
         self.view.backgroundColor = UIColor(hex: "F5F5F5", alpha: 1.0)
         
