c195bb14054739e3d10fdc92e3a7b0cc96a98052
原口和音 <kazunesaiko@yahoo.co.jp>
2018-08-02 19:32:32 +0900

    [add] added a custom view

---
+++ aboon/Classes/Shop/View/ShopDetailView.swift
@@ -0,0 +1,15 @@
+//
+//  ShopDetailView.swift
+//  aboon
+//
+//  Created by 原口和音 on 2018/08/02.
+//  Copyright © 2018年 aboon. All rights reserved.
+//
+
+import UIKit
+
+class ShopDetailView: UIView {
+
+    
+    
+}
