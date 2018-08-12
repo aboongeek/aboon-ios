74184c157705c749346e82fa7ae3ea18f55c9fc9
原口和音 <kazunesaiko@yahoo.co.jp>
2018-08-13 02:34:04 +0900

    [add] added Coupon and MyCoupon classes

---
+++ aboon/Models/Coupon.swift
@@ -0,0 +1,24 @@
+//
+//  Coupon.swift
+//  aboon
+//
+//  Created by 原口和音 on 2018/08/12.
+//  Copyright © 2018年 aboon. All rights reserved.
+//
+
+import Foundation
+
+class Coupon {
+    var imagePath = String()
+    var name = String()
+    var description = String()
+    var minimum = Int()
+    
+    init(imagePath: String, name: String, description: String, minimum: Int) {
+        self.imagePath = imagePath
+        self.name = name
+        self.description = description
+        self.minimum = minimum
+    }
+}
+
---
+++ aboon/Models/MyCoupon.swift
@@ -0,0 +1,21 @@
+//
+//  MyCoupon.swift
+//  aboon
+//
+//  Created by 原口和音 on 2018/08/12.
+//  Copyright © 2018年 aboon. All rights reserved.
+//
+
+import Foundation
+
+class MyCoupon: Coupon {
+    var isAvailable = false
+    var roomId = String()
+    
+    init(imagePath: String, name: String, description: String, minimum: Int, isAvailable: Bool, roomId: String) {
+        self.isAvailable = isAvailable
+        self.roomId = roomId
+        super.init(imagePath: imagePath, name: name, description: description, minimum: minimum)
+    }
+}
+
